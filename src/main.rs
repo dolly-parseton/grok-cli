28b20bc73b6de476359d6b21b467221091050693
dolly-parseton <dolly-parseton@gmail.com>
2021-03-15 23:24:11 +0000

    Added CSV and JSON parsing plus tidied code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-// 
+//
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-// 
+//
 
 mod glob {
     //! Functions to handle reading data from file.
+    use super::Result;
     use glob::glob;
-    use super::*;
     use std::{
         fs,
         io::{prelude::*, BufReader},
@@ -21,82 +21,44 @@ mod glob {
     impl FileInput {
         /// Creates a file FileInput struct, will panic if glob fails to be created and print error.
         pub fn new(glob_str: &str) -> Result<Self> {
-            let mut paths = match glob(glob_str)?;
+            let mut paths = glob(glob_str)?;
             let first_path = match paths.next() {
-                Some(res) => match res {
-                    Ok(p) => p,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        std::process::exit(1);
-                    }
-                },
-                None => {
-                    eprintln!("{} did not return any files", glob_str);
-                    std::process::exit(1);
-                }
+                Some(res) => res?,
+                None => return Err(format!("{} did not return any files", glob_str).into()),
             };
             // Create a reader from first glob
-            let file = match fs::OpenOptions::new().read(true).open(first_path) {
-                Ok(p) => p,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    std::process::exit(1);
-                }
-            };
+            let file = fs::OpenOptions::new().read(true).open(first_path)?;
             let current_reader = BufReader::new(file);
-            Self {
+            // Return Self
+            Ok(Self {
                 // glob_str: glob_str.to_string(),
                 paths,
                 current_reader,
-            }
+            })
         }
-    }
 
-    impl Iterator for FileInput {
-        type Item = String;
-
-        fn next(&mut self) -> Option<Self::Item> {
+        pub fn read_line(&mut self) -> Result<Option<String>> {
             // Check self.current_reader for data
             let mut line = String::new();
-            match self.current_reader.read_line(&mut line) {
-                Ok(_) => (),
-                Err(_e) => {
-                    match self.paths.next() {
-                        Some(p) => {
-                            // Create a BufReader
-                            let file = match fs::OpenOptions::new().read(true).open(match p {
-                                Ok(p) => p,
-                                Err(e) => {
-                                    eprintln!("{}", e);
-                                    std::process::exit(1);
-                                }
-                            }) {
-                                Ok(p) => p,
-                                Err(e) => {
-                                    eprintln!("{}", e);
-                                    std::process::exit(1);
-                                }
-                            };
-                            let mut reader = BufReader::new(file);
-                            // Read line
-                            let mut line = String::new();
-                            let _ = match reader.read_line(&mut line) {
-                                Ok(p) => p,
-                                Err(e) => {
-                                    eprintln!("{}", e);
-                                    std::process::exit(1);
-                                }
-                            };
-                            // Store reader
-                            self.current_reader = reader;
-                        }
-                        None => return None,
+            if self.current_reader.read_line(&mut line)? == 0 {
+                match self.paths.next() {
+                    Some(p) => {
+                        // Create a BufReader
+                        let file = fs::OpenOptions::new().read(true).open(p?)?;
+                        let mut reader = BufReader::new(file);
+                        // Read line
+                        let mut line = String::new();
+                        let _ = reader.read_line(&mut line)?;
+                        // Store reader
+                        self.current_reader = reader;
                     }
+                    None => return Ok(None),
                 }
-            };
+            }
+
             match line.is_empty() {
-                false => Some(line),
-                true => None,
+                false => Ok(Some(line)),
+                true => Ok(None),
             }
         }
     }
@@ -104,9 +66,10 @@ mod glob {
 
 mod grok_parser {
     //! Grok parser
+    use super::Result;
     use grok::{Grok, Pattern};
     use std::{
-        collections::HashMap,
+        collections::BTreeMap,
         fs,
         io::{prelude::*, BufReader},
         path::PathBuf,
@@ -120,12 +83,15 @@ mod grok_parser {
     }
 
     impl GrokParser {
-        pub fn new(pattern: &str, patterns: Option<&PathBuf>) -> Self {
+        pub fn new(pattern: &str, patterns: Option<&PathBuf>, no_patterns: bool) -> Result<Self> {
             let mut grok = match patterns {
                 Some(d) => {
                     //
-                    let mut g = Grok::empty();
-                    for (k, v) in read_aliases(d) {
+                    let mut g = match no_patterns {
+                        true => Grok::empty(),
+                        false => Grok::with_patterns(),
+                    };
+                    for (k, v) in read_aliases(d)? {
                         g.insert_definition(k, v);
                     }
                     g
@@ -133,60 +99,49 @@ mod grok_parser {
                 None => Grok::with_patterns(),
             };
             //
-            let pattern = match grok.compile(pattern, true) {
-                Ok(p) => p,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    std::process::exit(1);
-                }
-            };
+            let pattern = grok.compile(pattern, true)?;
             //
-            Self { grok, pattern }
+            Ok(Self { grok, pattern })
         }
 
-        pub fn parse(&self, data: &str) -> HashMap<String, String> {
+        pub fn parse(&self, data: &str) -> Result<BTreeMap<String, String>> {
             match self.pattern.match_against(data) {
-                None => HashMap::new(),
+                None => Err(format!("No matches against data: {}", data.trim_end()).into()),
                 Some(matches) => {
-                    let mut map = HashMap::new();
+                    let mut map = BTreeMap::new();
                     for (k, v) in matches.iter() {
                         map.insert(k.to_string(), v.to_string());
                     }
-                    map
+                    Ok(map)
                 }
             }
         }
     }
 
-    fn read_aliases(patterns: &PathBuf) -> Result<HashMap<String, String> {
-        let mut aliases = HashMap::new();
+    fn read_aliases(patterns: &PathBuf) -> Result<BTreeMap<String, String>> {
+        let mut aliases = BTreeMap::new();
         if !patterns.is_dir() && patterns.exists() {
-            eprintln!("{} patterns directory does not exist.", patterns.display());
-            std::process::exit(1);
+            return Err(
+                format!("{} patterns directory does not exist.", patterns.display()).into(),
+            );
         }
         //
-        for file in match fs::read_dir(patterns) {
-            Ok(p) => p,
-            Err(e) => {
-                eprintln!("{}", e);
-                std::process::exit(1);
-            }
-        } {
+        for file in fs::read_dir(patterns)? {
+            let file = file?;
             // Read lines in file and add them as patterns
-            if let Ok(f) = file {
-                let meta = f.metadata().unwrap();
-                if meta.is_file() {
-                    let reader = BufReader::new(fs::File::open(f.path()).unwrap());
-                    for line in reader.lines() {
-                        if let Ok(l) = line {
-                            let (key, value) = l.split_at(l.find(" ").unwrap());
-                            aliases.insert(key.to_string(), value.to_string());
-                        }
+
+            let meta = file.metadata().unwrap();
+            if meta.is_file() {
+                let reader = BufReader::new(fs::File::open(file.path()).unwrap());
+                for line in reader.lines() {
+                    if let Ok(l) = line {
+                        let (key, value) = l.split_at(l.find(' ').unwrap());
+                        aliases.insert(key.to_string(), value.trim_start().to_string());
                     }
                 }
             }
         }
-        aliases
+        Ok(aliases)
     }
 }
 
@@ -196,7 +151,7 @@ mod args {
     use structopt::StructOpt;
 
     #[derive(Debug, StructOpt)]
-    #[structopt(name = "example", about = "An example of StructOpt usage.")]
+    #[structopt(name = "grok", about = "Parse structured data using grok filters.")]
     pub struct Options {
         /// Pattern to match on
         #[structopt(short, long)]
@@ -209,20 +164,75 @@ mod args {
         pub output: Option<PathBuf>,
         /// Custom patterns directory, uses defaults is not provided.
         #[structopt(long, parse(from_os_str))]
-        pub patterns_dir: Option<PathBuf>,
+        pub patterns: Option<PathBuf>,
+        /// If this option is provided then the grok parsers will not populate the grok parser with all the default patterns.
+        #[structopt(long)]
+        pub no_patterns: bool,
+        /// Print CSV formatted data. Support for optional fields currently not yet implemented.
+        #[structopt(short, long)]
+        pub csv: bool,
+        /// Print JSON formatted data
+        #[structopt(short, long)]
+        pub json: bool,
     }
 }
 
 fn main() {
     use structopt::StructOpt;
     let opt = args::Options::from_args();
-    println!("{:?}", opt);
-
-    let mut file_in = glob::FileInput::new(&opt.input);
-    let grok_parser = grok_parser::GrokParser::new(&opt.pattern, opt.patterns_dir.as_ref());
-
-    while let Some(a) = file_in.next() {
-        println!("{:?}", grok_parser.parse(&a));
-        // std::process::exit(1);
+    // Check json and csv options are not both being used.
+    if opt.json && opt.csv {
+        eprintln!("Select either JSON or CSV but not both options to output");
+        std::process::exit(1);
+    }
+    // Get a file input handle.
+    let mut file_in = match glob::FileInput::new(&opt.input) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("FileInput: {}", e);
+            std::process::exit(1);
+        }
+    };
+    // Get Grok parser parser. Handle based on options.
+    let grok_parser =
+        match grok_parser::GrokParser::new(&opt.pattern, opt.patterns.as_ref(), opt.no_patterns) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("GrokParser: {}", e);
+                std::process::exit(1);
+            }
+        };
+    //
+    let mut headers = Vec::new();
+    while let Ok(Some(a)) = file_in.read_line() {
+        let parsed = match grok_parser.parse(&a) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("GrokParser: {}", e);
+                continue;
+            }
+        };
+        // Print as CSV if option is true
+        if opt.csv {
+            if headers.is_empty() {
+                headers = parsed.keys().map(|k| format!("\"{}\"", k)).collect();
+                headers.sort();
+                println!("{}", headers.join(", "));
+            }
+            // Parse in order of vec, otherwise the resulting values are unordered.
+            let values: Vec<String> = parsed.values().map(|v| format!("\"{}\"", v)).collect();
+            println!("{}", values.join(", "));
+        }
+        // Print as JSON if option is true
+        if opt.json {
+            let json = match serde_json::to_string(&parsed) {
+                Ok(p) => p,
+                Err(e) => {
+                    eprintln!("{}", e);
+                    continue;
+                }
+            };
+            println!("{}", json);
+        }
     }
 }
