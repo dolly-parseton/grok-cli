acc714ec677009552a9f5517945e82ccfd368222
dolly-parseton <dolly-parseton@gmail.com>
2021-03-21 14:33:28 +0000

    Added more features and improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 //
+#[macro_use]
+extern crate serde;
+//
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 //
 
@@ -64,6 +67,76 @@ mod glob {
     }
 }
 
+mod outputs {
+    //! Output components (file and print to start)
+    //! Output Trait for ensuring Box<dyn Output> is possible
+    use super::Result;
+    use std::{fs, path::PathBuf};
+    pub trait Output {
+        fn output(&self, parse_res: Result<String>) -> Result<()>;
+    }
+    // Print output
+    pub struct PrintOutput;
+    impl Output for PrintOutput {
+        fn output(&self, parse_res: Result<String>) -> Result<()> {
+            match parse_res {
+                Ok(o) => println!("{}", o),
+                Err(e) => eprintln!("{}", e),
+            }
+            Ok(())
+        }
+    }
+    // File output
+    pub struct FileOutput {
+        path: PathBuf,
+        err_path: PathBuf,
+    }
+    impl FileOutput {
+        pub fn new(path: PathBuf) -> Result<Self> {
+            let err_path = path.join(".err");
+            Err(match (err_path.exists(), !path.exists()) {
+                (true, true) => format!(
+                    "Could not log to {} or {}. Both files already exist.",
+                    err_path.display(),
+                    path.display()
+                ),
+                (false, true) => {
+                    format!("Could not log to {}, file already exists", path.display())
+                }
+                (true, false) => format!(
+                    "Could not log to {}, file already exists",
+                    err_path.display()
+                ),
+                (false, false) => return Ok(Self { err_path, path }),
+            }
+            .into())
+        }
+    }
+    impl Output for FileOutput {
+        fn output(&self, parse_res: Result<String>) -> Result<()> {
+            use std::io::Write;
+            match parse_res {
+                Ok(o) => {
+                    let mut file = fs::OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .open(&self.path)?;
+                    let _ = file.write(o.as_bytes())?;
+                    let _ = file.write("\n".as_bytes())?;
+                }
+                Err(e) => {
+                    let mut file = fs::OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .open(&self.err_path)?;
+                    let _ = file.write(format!("{}\n", e.to_string()).as_bytes())?;
+                }
+            }
+            Ok(())
+        }
+    }
+}
+
 mod grok_parser {
     //! Grok parser
     use super::Result;
@@ -104,10 +177,18 @@ mod grok_parser {
             Ok(Self { grok, pattern })
         }
 
-        pub fn parse(&self, data: &str) -> Result<BTreeMap<String, String>> {
+        pub fn parse(
+            &self,
+            data: &str,
+            stats: &mut super::stats::Stats,
+        ) -> Result<BTreeMap<String, String>> {
             match self.pattern.match_against(data) {
-                None => Err(format!("No matches against data: {}", data.trim_end()).into()),
+                None => {
+                    stats.failed += 1;
+                    Err(format!("No matches against data: \"{}\"", data.trim_end()).into())
+                }
                 Some(matches) => {
+                    stats.parsed += 1;
                     let mut map = BTreeMap::new();
                     for (k, v) in matches.iter() {
                         map.insert(k.to_string(), v.to_string());
@@ -145,11 +226,18 @@ mod grok_parser {
     }
 }
 
+mod stats {
+    #[derive(Default, Serialize)]
+    pub struct Stats {
+        pub parsed: u64,
+        pub failed: u64,
+    }
+}
+
 mod args {
     //! StructOpt argument functions.
     use std::path::PathBuf;
     use structopt::StructOpt;
-
     #[derive(Debug, StructOpt)]
     #[structopt(name = "grok", about = "Parse structured data using grok filters.")]
     pub struct Options {
@@ -159,7 +247,7 @@ mod args {
         /// Input file glob, can match on multiple files.
         #[structopt(short, long)]
         pub input: String,
-        /// Output file, stdout if not provided
+        /// Output file, stdout if not provided.
         #[structopt(short, long, parse(from_os_str))]
         pub output: Option<PathBuf>,
         /// Custom patterns directory, uses defaults is not provided.
@@ -168,16 +256,23 @@ mod args {
         /// If this option is provided then the grok parsers will not populate the grok parser with all the default patterns.
         #[structopt(long)]
         pub no_patterns: bool,
-        /// Print CSV formatted data. Support for optional fields currently not yet implemented.
+        /// Return CSV formatted data. Support for optional fields currently not yet implemented.
         #[structopt(short, long)]
         pub csv: bool,
-        /// Print JSON formatted data
+        /// Return JSON formatted data.
         #[structopt(short, long)]
         pub json: bool,
+        /// Return stats on printing, number of successfully parsed and failed records.
+        #[structopt(short, long)]
+        pub stats: bool,
+        // Todo features
+        // * Pattern Dictionary (ie. A root pattern and varients)
+        // * Matching multiple patterns and testing for a match
+        // * Stats on matched and failed lines and logging failures (or eprint if no output file is provided)
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     use structopt::StructOpt;
     let opt = args::Options::from_args();
     // Check json and csv options are not both being used.
@@ -202,13 +297,26 @@ fn main() {
                 std::process::exit(1);
             }
         };
-    //
-    let mut headers = Vec::new();
-    while let Ok(Some(a)) = file_in.read_line() {
-        let parsed = match grok_parser.parse(&a) {
+    // Get the generic output struct.
+    let output: Box<dyn outputs::Output> = match opt.output {
+        Some(p) => Box::new(match outputs::FileOutput::new(p) {
             Ok(p) => p,
             Err(e) => {
-                eprintln!("GrokParser: {}", e);
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        }),
+        None => Box::new(outputs::PrintOutput),
+    };
+    // Generate a stats component.
+    let mut stats = stats::Stats::default();
+    let mut headers = Vec::new();
+    while let Ok(Some(a)) = file_in.read_line() {
+        let parsed = match grok_parser.parse(&a, &mut stats) {
+            Ok(p) => p,
+            Err(e) => {
+                output.output(Err(e))?;
+                // eprintln!("GrokParser: {}", e);
                 continue;
             }
         };
@@ -217,22 +325,41 @@ fn main() {
             if headers.is_empty() {
                 headers = parsed.keys().map(|k| format!("\"{}\"", k)).collect();
                 headers.sort();
-                println!("{}", headers.join(", "));
+                output.output(Ok(headers.join(", ").to_string()))?;
+                // println!("{}", headers.join(", "));
             }
             // Parse in order of vec, otherwise the resulting values are unordered.
             let values: Vec<String> = parsed.values().map(|v| format!("\"{}\"", v)).collect();
-            println!("{}", values.join(", "));
+            output.output(Ok(values.join(", ").to_string()))?;
+            // println!("{}", values.join(", "));
         }
         // Print as JSON if option is true
         if opt.json {
             let json = match serde_json::to_string(&parsed) {
                 Ok(p) => p,
                 Err(e) => {
-                    eprintln!("{}", e);
+                    output.output(Err(Box::new(e)))?;
                     continue;
                 }
             };
-            println!("{}", json);
+            output.output(Ok(json))?;
         }
     }
+    // If stats flag was selectedf output the stats data
+    // Print as CSV if option is true
+    if opt.csv {
+        output.output(Ok(vec!["parsed", "failed"].join(", ")))?;
+        output.output(Ok(
+            vec![stats.parsed.to_string(), stats.failed.to_string()].join(", ")
+        ))?;
+        // println!("{}", values.join(", "));
+    }
+    // Print as JSON if option is true
+    if opt.json {
+        match serde_json::to_string(&stats) {
+            Ok(p) => output.output(Ok(p))?,
+            Err(e) => output.output(Err(Box::new(e)))?,
+        }
+    }
+    Ok(())
 }
